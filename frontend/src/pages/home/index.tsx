d343f81b220ee193c73cb4106732fd6075b17a09
Niranjan Patil <103982890+NiranjanPatil07@users.noreply.github.com>
2024-02-25 20:42:42 +0530

    UX: Enhancement

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,20 +1,11 @@
 // import TrackCard from '@/components/card/TrackCard'
-import { Button } from '@/components/ui/button'
-import { useGetRequestQuery } from '@/contexts/api/soundtrackApiService'
 import { FC } from 'react'
-import { useNavigate } from 'react-router-dom'
 const Home: FC = () => {
-  const navigate = useNavigate()
-
-  const { data } = useGetRequestQuery('/spotify/search')
-  console.log('🚀 ~ testDAta:', data)
   return (
     <>
-      <div className='flex justify-between items-center w-full mb-4 '>
+      <div className='mb-4 flex w-full items-center justify-between '>
         <h1>Home</h1>
-        <Button onClick={() => navigate('/sign-in')}>Log In</Button>
       </div>
-      {/* <TrackCard data={}/> */}
     </>
   )
 }
