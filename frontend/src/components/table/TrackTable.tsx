a55cd443e4b8d237295fbe3b5487fabad6e155b7
Niranjan Patil <103982890+NiranjanPatil07@users.noreply.github.com>
2024-03-14 20:15:44 +0530

    UX: Enhancement

--- frontend/src/components/table/TrackTable.tsx
+++ frontend/src/components/table/TrackTable.tsx
@@ -1,5 +1,5 @@
 import { Table, TableBody, TableCaption, TableCell, TableRow } from '@/components/ui/table'
-import { SmallArtistType, TrackType } from '@/utils/types/type'
+import { ArtistType, SmallArtistType, TrackType } from '@/utils/types/type'
 import { Dot, Heart, MoreHorizontal, PlayIcon } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router'
@@ -19,51 +19,54 @@ const TrackTable: React.FC<TrackTableProps> = ({ tableTile, data, hideImage }) =
     return `${minutes}:${parseInt(seconds) < 10 ? '0' : ''}${seconds}`
   }
 
+  const displayArtistList = (artist: any) => {
+    return artist
+      ?.map((item: ArtistType) => item?.name)
+      ?.join(' • ')
+      ?.toString()
+  }
+
   return (
     <>
       <TableCaption className='flex pb-2 text-2xl font-bold text-foreground'>{tableTile}</TableCaption>
       <Table>
         <TableBody className='space-y-4'>
           {data &&
-            data?.map((row: any, index: number) => (
-              <TableRow draggable={false} className='group overflow-x-auto' key={index}>
-                <TableCell className='w-10 select-none text-center  transition-all md:w-16'>
-                  <div className='flex items-center justify-center'>
-                    <p className='group-hover:hidden'>{index + 1}</p>
-                    <PlayIcon className='hidden cursor-pointer text-foreground/80 group-hover:block' size={'20'} />
-                  </div>
-                </TableCell>
-                <TableCell className='flex items-center space-x-2 py-3 md:space-x-3'>
-                  {!hideImage && (
-                    <img src={row?.album?.images?.find((val: any) => val?.height === 64)?.url} alt='' className='size-10 md:size-10' />
-                  )}
-                  <div>
-                    <p
-                      className='line-clamp-1 min-w-44 select-none text-sm hover:cursor-pointer hover:underline sm:text-base '
-                      onClick={() => navigate(`/track/${row?.id}`)}
-                    >
-                      {row?.name}
-                    </p>
-                    <div className='flex items-center space-x-1 '>
-                      {row?.artists?.map((artist: SmallArtistType, index: number) => (
-                        <>
-                          {index > 0 ? <Dot size={15} className='text-muted-foreground' /> : null}
-                          <p className='text-[0.70rem] text-muted-foreground'>{artist?.name}</p>
-                        </>
-                      ))}
+            data?.map((row: any, index: number) => {
+              const ARTIST_LIST = displayArtistList(row?.artists)
+              return (
+                <TableRow draggable={false} className='group overflow-x-auto' key={index}>
+                  <TableCell className='w-10 select-none text-center  transition-all md:w-16'>
+                    <div className='flex items-center justify-center'>
+                      <p className='group-hover:hidden'>{index + 1}</p>
+                      <PlayIcon className='hidden size-4 cursor-pointer text-foreground/80 group-hover:block md:size-5' />
                     </div>
-                  </div>
-                </TableCell>
-                <TableCell className='hidden select-none'>{row?.artists?.map((val: any) => val?.name)?.join(', ')}</TableCell>
-                <TableCell className=''>
-                  <Heart size={18} className='hidden hover:cursor-pointer md:table-cell' />
-                </TableCell>
-                <TableCell className='select-none text-xs md:text-sm'>{formatTime(row?.durationMs)}</TableCell>
-                <TableCell className='px-2'>
-                  <MoreHorizontal size={20} className='hover:cursor-pointer' />
-                </TableCell>
-              </TableRow>
-            ))}
+                  </TableCell>
+                  <TableCell className='flex items-center space-x-2 py-3 md:space-x-3'>
+                    {!hideImage && (
+                      <img src={row?.album?.images?.find((val: any) => val?.height === 64)?.url} alt='' className='size-10 md:size-10' />
+                    )}
+                    <div>
+                      <p
+                        className='line-clamp-1 min-w-44 select-none text-sm hover:cursor-pointer hover:underline sm:text-base '
+                        onClick={() => navigate(`/track/${row?.id}`)}
+                      >
+                        {row?.name} {row?.name}
+                      </p>
+                      <p className='line-clamp-1 text-[0.70rem] text-muted-foreground'>{ARTIST_LIST}</p>
+                    </div>
+                  </TableCell>
+                  <TableCell className='hidden select-none'>{row?.artists?.map((val: any) => val?.name)?.join(', ')}</TableCell>
+                  <TableCell className=''>
+                    <Heart size={18} className='hidden hover:cursor-pointer md:table-cell' />
+                  </TableCell>
+                  <TableCell className='select-none text-xs md:text-sm'>{formatTime(row?.durationMs)}</TableCell>
+                  <TableCell className='px-2'>
+                    <MoreHorizontal size={20} className='hover:cursor-pointer' />
+                  </TableCell>
+                </TableRow>
+              )
+            })}
         </TableBody>
       </Table>
     </>
